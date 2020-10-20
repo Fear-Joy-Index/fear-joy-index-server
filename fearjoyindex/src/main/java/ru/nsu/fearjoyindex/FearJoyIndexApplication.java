24b51e6ca69e7dfe6483ecfa5c9fa618f1e88bdd
alenakokorina24 <alena.kokorina24@gmail.com>
2020-10-21 01:11:03 +0700

    created spring boot project and configured pom.xml

---
+++ fearjoyindex/src/main/java/ru/nsu/fearjoyindex/FearJoyIndexApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.fearjoyindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FearJoyIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FearJoyIndexApplication.class, args);
+    }
+
+}
---
+++ fearjoyindex/src/test/java/ru/nsu/fearjoyindex/FearjoyindexApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.fearjoyindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FearjoyindexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
